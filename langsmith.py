625bf0fc0438e2fbb94042616929554d29c0e6ec
Richard E. Robbins <RERobbins@iTinker.net>
2023-09-16 18:01:53 -0700

    langsmith

---
+++ langsmith.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+from langchain.chat_models import ChatOpenAI
+
+#load_dotenv(find_dotenv(".langsmith_env"))
+load_dotenv(find_dotenv())
+
+vars = ["LANGCHAIN_TRACING_V2",
+        "LANGCHAIN_ENDPOINT",
+        "LANGCHAIN_API_KEY",
+        "LANGCHAIN_PROJECT"]
+
+for var in vars:
+    print(f"({var}: {os.getenv(var)}")
